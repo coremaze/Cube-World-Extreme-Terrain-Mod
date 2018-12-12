f3a7cafc09302f360c097261fc7f7cbcbceffeb5
ChrisMiuchiz <chris000005@gmail.com>
2018-12-11 22:11:56 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,48 +1,57 @@
-#undef __STRICT_ANSI__
-#include "main.h"
+#include <windows.h>
 #include <math.h>
 
-UINT_PTR base;
-double height;
-double YNoise = 0.01;
+unsigned int base;
 
+double VanillaNoise(double x, double y)
+{
+  double v3;
+  double v4;
+  double v5;
+  int v6;
+  signed int v7;
+  int v8;
+  int v9;
+  int v10;
+  int v11;
+  double v12;
+  double v13;
+  int v15;
 
-_declspec(naked) void DLL_EXPORT ASMMultiplier(){
-    asm("call [_HeightMultiplierPtr]");
-    asm("movsd [ebp + 0x10], xmm3");
-
-    asm("mov eax, [_base]"); //jump  back
-    asm("add eax, 0xC10ED");
-    asm("jmp eax");
+  v3 = x;
+  v4 = (double)(signed int)y;
+  v5 = (double)(signed int)x;
+  v6 = 57 * (signed int)v4;
+  v7 = (signed int)(v5 + 1.0);
+  v8 = (((signed int)v5 + v6) << 13) ^ ((signed int)v5 + v6);
+  v15 = ((v7 + v6) << 13) ^ (v7 + v6);
+  v9 = 57 * (signed int)(v4 + 1.0);
+  v10 = ((v9 + (signed int)v5) << 13) ^ (v9 + (signed int)v5);
+  v11 = ((v7 + v9) << 13) ^ (v7 + v9);
+  v12 = (1.0 - cos((v3 - v5) * 3.1415927)) * 0.5;
+  v13 = cos((y - v4) * 3.1415927);
+  return (float)(((1.0 - (double)((v8 * (60493 * v8 * v8 + 19990303) - 771171059) & 0x7FFFFFFF) * 9.313225746154785e-10)
+                * (1.0 - v12)
+                + (1.0
+                 - (double)((v15 * (60493 * v15 * v15 + 19990303) - 771171059) & 0x7FFFFFFF) * 9.313225746154785e-10)
+                * v12)
+               * (1.0 - (1.0 - v13) * 0.5)
+               + ((1.0
+                 - (double)((v10 * (60493 * v10 * v10 + 19990303) - 771171059) & 0x7FFFFFFF) * 9.313225746154785e-10)
+                * (1.0 - v12)
+                + (1.0
+                 - (double)((v11 * (60493 * v11 * v11 + 19990303) - 771171059) & 0x7FFFFFFF) * 9.313225746154785e-10)
+                * v12)
+               * ((1.0 - v13)
+                * 0.5));
 }
 
-_declspec(naked) void DLL_EXPORT ASMYNoise1(){
-    asm(" mulsd xmm1, [_YNoise]");
-    asm("cvtdq2pd xmm2, xmm2");
 
-    asm("mov eax, [_base]"); //jump  back
-    asm("add eax, 0x1EE5B4");
-    asm("jmp eax");
+double __cdecl GenerateNoise(double x, double y){
+    double vn = VanillaNoise(x, y);
+    //apply a sigmoid function
+    return 2.0 * (1.0/(1.0 + pow(2, -6.0 * vn))) - 1.0;
 }
-
-_declspec(naked) void DLL_EXPORT ASMYNoise2(){
-    asm("mulsd xmm2, [_YNoise]");
-    asm("movaps xmm0, xmm1");
-
-    asm("mov eax, [_base]"); //jump  back
-    asm("add eax, 0x1EE5C4");
-    asm("jmp eax");
-}
-
-void DLL_EXPORT HeightMultiplier(){
-    asm("movsd [_height], xmm3");
-
-    height *= 1.4;
-
-    asm("movsd xmm3, [_height]");
-}
-DWORD HeightMultiplierPtr = (DWORD)&HeightMultiplier;
-
 void WriteJMP(BYTE* location, BYTE* newFunction){
 	DWORD dwOldProtection;
 	VirtualProtect(location, 5, PAGE_EXECUTE_READWRITE, &dwOldProtection);
@@ -51,22 +60,18 @@ void WriteJMP(BYTE* location, BYTE* newFunction){
 	VirtualProtect(location, 5, dwOldProtection, &dwOldProtection);
 }
 
-
-extern "C" DLL_EXPORT BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+extern "C" __declspec(dllexport) bool APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
-    base = (UINT_PTR)GetModuleHandle(NULL);
     switch (fdwReason)
     {
-
         case DLL_PROCESS_ATTACH:
-            WriteJMP((BYTE*)(base + 0xC10E8), (BYTE*)&ASMMultiplier);
+            base = (unsigned int)GetModuleHandle(NULL);
 
-            //WriteJMP((BYTE*)(base + 0x1EE5A8), (BYTE*)&ASMYNoise1);
-
-            //WriteJMP((BYTE*)(base + 0x1EE5B9), (BYTE*)&ASMYNoise2);
+            //overwrite beginning of vanilla noise function
+            WriteJMP((BYTE*)(base + 0x0C0EF0), (BYTE*)&GenerateNoise);
 
             break;
-;
+
     }
-    return TRUE;
+    return true;
 }
